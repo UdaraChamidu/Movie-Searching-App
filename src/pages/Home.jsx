c6ba367d92371b5a98c9fcb579510f30b2ca1a98
udara <chamiduudara321@gmail.com>
2025-10-04 23:52:28 +0530

    all files added

---
+++ src/pages/Home.jsx
@@ -0,0 +1,45 @@
+import MovieCard from "../components/MovieCard";
+
+import { useState } from "react";
+
+function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const movies = [
+    { id: 1, title: "Inception", year: "2010" },
+    { id: 2, title: "Interstellar", year: "2014" },
+    { id: 3, title: "The Dark Knight", year: "2008" },
+  ];
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert("Searching for: " + searchQuery);
+    setSearchQuery("");
+  };
+
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for a movie..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+
+      <div className="movies-grid">
+        {movies.map((movie) => 
+        // movie.title.toLowerCase().startsWith(searchQuery) &&
+        (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  );
+}
+export default Home;
