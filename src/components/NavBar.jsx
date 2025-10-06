5ff79aa5df2a03c70e9bfe95039e98980ea7a213
udara <chamiduudara321@gmail.com>
2025-10-06 14:43:46 +0530

    navbar edited

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,7 +9,7 @@ function NavBar(){
             </div>
             <div className="navbar-links">
                 <Link to="/" className="nav-link">Home</Link>
-                <Link to="/favourites" className="nav-link">Favourites</Link>
+                <Link to="/favorites" className="nav-link">Favourites</Link>
             </div>
         </nav>
     );
